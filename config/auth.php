416f809c9d7d0ef883c7e5134e050d7fe89a0d5d
BRIJALDO <67452224+BRIJALDO@users.noreply.github.com>
2020-10-09 22:37:59 -0500

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Defaultsa
+    | Authentication Defaults
     |--------------------------------------------------------------------------
     |
     | This option controls the default authentication "guard" and password
